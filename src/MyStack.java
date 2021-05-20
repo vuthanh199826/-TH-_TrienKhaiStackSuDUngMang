0067f420f4919200c2ae756894863833cb5c5c10
vuthanh199826 <vuthanh199825@gmail.com>
2021-05-20 16:46:40 +0700

    done

---
+++ src/MyStack.java
@@ -0,0 +1,38 @@
+public class MyStack {
+    private int arr[];
+    private int size;
+    private int index = 0;
+
+    public MyStack(int size) {
+        this.size = size;
+        arr = new int[size];
+    }
+    public boolean isFull() {
+        if (index == size) {
+            return true;
+        }
+        return false;
+    }
+    public boolean isEmpty() {
+        if (index == 0) {
+            return true;
+        }
+        return false;
+    }
+    public int size() {
+        return index;
+    }
+    public void push(int element){
+        if (isFull()) {
+            throw new StackOverflowError("Stack is full");
+        }
+        arr[index] = element;
+        index++;
+    }
+    public int pop() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Stack is null");
+        }
+        return arr[--index];
+    }
+}
